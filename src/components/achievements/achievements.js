d2090882be624e351df673a662edbcbdf310726c
iYung <ivankhyung@gmail.com>
2017-06-21 16:01:58 +0000

    added more achievements

--- src/components/achievements/achievements.js
+++ src/components/achievements/achievements.js
@@ -15,21 +15,23 @@ const Achievement = props => { return (
 
 const Page2017 = () => (
   <div>
-  <Achievement name="Good Stuff" date="Sometime" txt="We won I think" /><Divider/>
-  <Achievement name="Good Stuff" date="Oneday" txt="We won I think" />
+  <Achievement name="Best Domain Name from Domain.com @ ConUHacks" date="January" txt="This award was won by my team in a hackathon hosted at Concordia University. We won with our Raspberry Pi project called Theia against 7 other submissions. The award was given due to our punny domain name for our webpage hosted by Azure." /><Divider/>
+  <Achievement name="Best .Tech Domain @ ConUHacks" date="January" txt="This award was won by my team in a hackathon hosted at Concordia University. We won with our Raspberry Pi project called Theia against 7 other submissions. The award was given due to our punny domain name for our webpage hosted by Azure." /><Divider/>
+  <Achievement name="Second Place @ QHacks" date="February" txt="This award was won by my team in a hackathon hosted at Queen's University. We won with our Raspberry Pi project called Green Garbage Bin against 84 other submissions. This project was a combination of our new skills from the past few hackathon projects." /><Divider/>
+  <Achievement name="Best Use of Indico's APIs @ QHacks" date="February" txt="This award was won by my team in a hackathon hosted at Queen's University. We won with our Raspberry Pi project called Green Garbage Bin against 12 other submissions. This project was a combination of our new skills from the past few hackathon projects." />
   </div>
 )
 
 const Page2016 = () => (
   <div>
-  <Achievement name="Third Place @ WearHacksKW" date="March" txt="This award was won by my team at a hackathon hosted in Waterloo's Accelerator Centre. We won with our AR project called AR Watch against 15 other submissions. This project was our first experience with augmented reality." /><Divider/>
-  <Achievement name="First Place @ HackWestern 3" date="October" txt="This award was won by my team at a yearly hackathon hosted at Western University. We won with our Raspberry Pi project called Braille Printer against 92 other submissions. This project was our first real experience with Python, solenoids, and the Raspberry Pi." />
+  <Achievement name="Third Place @ WearHacksKW" date="March" txt="This award was won by my team in a hackathon hosted in Waterloo's Accelerator Centre. We won with our AR project called AR Watch against 14 other submissions. This project was our first experience with augmented reality." /><Divider/>
+  <Achievement name="First Place @ HackWestern 3" date="October" txt="This award was won by my team in a hackathon hosted at Western University. We won with our Raspberry Pi project called Braille Printer against 91 other submissions. This project was our first real experience with Python, solenoids, and the Raspberry Pi." />
   </div>
 )
 
 const Page2015 = () => (
   <div>
-  <Achievement name="Best Hardware Hack @ HackWestern 2" date="November" txt="This award was won by my team at a yearly hackathon hosted at Western University. We won with our VR project called Just Out For a Rift against 24 other submissions. My team earned this award by combining hardware and software to provide an unique experience with a new emerging technology." />
+  <Achievement name="Best Hardware Hack @ HackWestern 2" date="November" txt="This award was won by my team in a hackathon hosted at Western University. We won with our VR project called Just Out For a Rift against 24 other submissions. My team earned this award by combining hardware and software to provide an unique experience with a new emerging technology." />
   </div>
 )
 
