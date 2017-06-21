72d5897e8ecf7888e3797a3aca2cd33263b20a41
iYung <ivankhyung@gmail.com>
2017-06-21 18:18:09 +0000

    fixed footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <p>
     <Container text textAlign={"center"} id="foot">
     <Divider/>
-      Made with  <a href="https://facebook.github.io/react/" target="_blank"><Image src="https://facebook.github.io/react/img/logo.svg" shape={"circular"} inline={true} size={"mini"} /></a> and <a href="https://facebook.github.io/react/" target="_blank"><Image src="https://react.semantic-ui.com" inline={true} size={"mini"} /></a>
+      Made with  <a href="https://facebook.github.io/react/" target="_blank"><Image src="https://facebook.github.io/react/img/logo.svg" shape={"circular"} inline={true} size={"mini"} /></a> and <a href="https://react.semantic-ui.com/" target="_blank"><Image src="https://react.semantic-ui.com/logo.png" inline={true} size={"mini"} /></a>
     </Container>
     </p>
   </div>
