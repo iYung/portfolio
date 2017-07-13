548cc495a453a5b7e243095688b4610886b978cd
iYung <ivankhyung@gmail.com>
2017-07-13 02:55:31 +0000

    set up mongoose

--- server.js
+++ server.js
@@ -1,43 +1,28 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+mongoose.connect('mongodb://admin:password@ds117889.mlab.com:17889/portfolio');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  console.log('activity');
-    next();
+  console.log('req made');
+  next();
 });
 
-var port = 3000;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
+var port = 3000;
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
