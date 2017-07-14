ec52e55f94d07a0c3cef208b43aa476fa0028aec
iYung <ivankhyung@gmail.com>
2017-07-14 15:04:59 +0000

    started achievement section of api

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var Achievement = require('./models/achievement');
+
 mongoose.connect('mongodb://admin:password@ds117889.mlab.com:17889/portfolio');
+
+var port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,12 +19,47 @@ app.use(function(req, res, next) {
   next();
 });
 
-var port = 3000;
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
+
+//achievements
+router.route('/achievements')
+
+    .post(function(req, res) {
+        var achievement = new Achievement();     
+        achievement.name = req.body.name;
+        achievement.date = req.body.date;
+        achievement.text = req.body.text;
+        achievement.year = req.body.year;
+        achievement.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Achievement created!' });
+        });
+    })
+    
+    .delete(function(req, res) {
+        Achievement.remove({
+            __v: 0
+        }, function(err, chord) {
+            if (err)
+                return res.send(err);
+            res.json({ message: 'Successfully deleted all achievements' });
+        });
+    });
+    
+router.route('/achievements/2016')
+
+    .get(function(req, res) {
+        Achievement.find(function(err, achievements) {
+            if (err)
+                res.send(err);
+            res.json(achievements);
+        });
+    });
 
 app.use('/api', router);
 
