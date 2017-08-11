580214715ee668f28216f921e59e90bbca167b7f
iYung <ivankhyung@gmail.com>
2017-08-11 13:22:48 +0000

    basic login feature added

--- client/src/components/admin/admin.js
+++ client/src/components/admin/admin.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react'
+import { Form, Segment, Label } from 'semantic-ui-react'
 import Axios from 'axios';
 import Qs from 'qs';
 
 import Dashboard from './dashboard'
 
-class Login extends Component { 
+class Login extends Component {
+  
+  login = () => {
+    const username = document.getElementById("loginUsername").value;
+    const password = document.getElementById("loginPassword").value;
+    Axios.post('/api/login', Qs.stringify({ 'username': username, 'password': password }))
+      .then(res => {
+        if (res.data.success) {
+          this.props.onclick();
+        } else {
+          alert("Login failed!");
+        }
+      });
+  }
+  
   render(){ 
-    
     return(
       <div>
         <Segment.Group>
@@ -18,13 +31,13 @@ class Login extends Component {
           <Form>
             <Form.Field>
               <label>Username</label>
-              <input placeholder='Username' />
+              <input id={"loginUsername"} placeholder='Username' />
             </Form.Field>
             <Form.Field>
               <label>Password</label>
-              <input placeholder='Password' />
+              <input id={"loginPassword"} placeholder='Password' />
             </Form.Field>
-            <Button onClick={this.props.onclick}>Login</Button>
+            <Label onClick={this.login}>Login</Label>
           </Form>
           </Segment>
         </Segment.Group>
