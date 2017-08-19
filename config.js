5783e8e1b4e2d5b46b1400080a1bd743208ec45d
iYung <ivankhyung@gmail.com>
2017-08-19 21:29:12 +0000

    config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'secret': '53CR3T_P455W0RD',
+    'database': 'mongodb://admin:password@ds117889.mlab.com:17889/portfolio',
+    'saltRounds': 10,
+    'serverPort': 3000
+};
