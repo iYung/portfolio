4f349225e856a12bf5740e34ba8549560da4f9d2
iYung <ivankhyung@gmail.com>
2017-08-22 04:40:27 +0000

    fixed config issue

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
     'secret': '53CR3T_P455W0RD',
-    'database': 'mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo for demo',
+    'database': 'mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo',
     'saltRounds': 10,
     'serverPort': 3000
 };
 
-//mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo for demo
+//prod
+//mongodb://admin:password@ds117889.mlab.com:17889/portfolio
+//demo
+//mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo
