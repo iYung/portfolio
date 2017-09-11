46c5947f39547ff0dce2de46a980407346346457
iYung <ivankhyung@gmail.com>
2017-09-11 05:19:58 +0000

    new build

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'secret': '53CR3T_P455W0RD',
+    'database': 'mongodb://admin:password@ds117889.mlab.com:17889/portfolio',
+    'saltRounds': 10,
+    'serverPort': 3000
+};
+
+//prod
+//mongodb://admin:password@ds117889.mlab.com:17889/portfolio
+//demo
+//mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo
+
+//how to update
+//build
+//update config
+//update .gitignores for build and config
+
+//pure-shore is live
+//hidden-waters is demo
