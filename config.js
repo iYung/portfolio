8185ee498de2cce23d63ec20db2f8c7f3bb76b65
iYung <ivankhyung@gmail.com>
2017-09-11 16:37:27 +0000

    fixing set up

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': '53CR3T_P455W0RD',
-    'database': 'mongodb://admin:password@ds153113.mlab.com:53113/portfoliodemo',
+    'database': 'mongodb://admin:password@ds117889.mlab.com:17889/portfolio',
     'saltRounds': 10,
     'serverPort': 3000
 };
